5123504677822d56c66d5e94f61d51c3de844982
Julio Rodriguez-Zambrano <228585521+rodBugo@users.noreply.github.com>
2025-12-15 01:16:51 -0700

    feat(rps-game): add function for random cpu choice

--- rps-game.js
+++ rps-game.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+/* This function gets the computer's choice at random. */
+function getComputerChoice() {
+    let cpuChoice = Math.floor(Math.random() * 3);
+
+    if (cpuChoice === 0) {
+        return "Rock";
+    } else if (cpuChoice === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
