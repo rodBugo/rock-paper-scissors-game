f33ced99aab2b58b6e789ad2a912ae7f375fe918
Julio Rodriguez-Zambrano <228585521+rodBugo@users.noreply.github.com>
2025-12-28 16:03:48 -0700

    feat: add function to check winner / update score

--- rps-game.js
+++ rps-game.js
@@ -41,56 +41,25 @@ function playRound(humanChoice, computerChoice) {
         computerScore++
         resultDiv.textContent = `You lose this round! ${computerChoice} beats ${humanChoice}`;
     }
+
+    updateScore();
+    checkWinner();
 }
 
 
-/* This function will play the game for five rounds. */
-function playGame() {
-    /* Initialize score */
-    let humanScore = 0;
-    let computerScore = 0;
-
-    /* This function will play a single round of rock, paper, scissors. */
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log(`It's a tie! both choose ${humanChoice}`);
-            console.log(humanScore, computerScore);
-            return;
-        } 
-
-        if (
-            (humanChoice === "Rock" && computerChoice === "Scissors") ||
-            (humanChoice === "Paper" && computerChoice === "Rock") ||
-            (humanChoice === "Scissors" && computerChoice === "Paper") 
-        ) {
-            humanScore++;
-            console.log(`You win this round! ${humanChoice} beats ${computerChoice}`);
-            console.log(humanScore, computerScore);
-            return;
-        }
-        else {
-            computerScore++
-            console.log(`You lost this round! ${computerChoice} beats ${humanChoice}`);
-            console.log(humanScore, computerScore);
-        }
-    }
-
-    /* Play five rounds */
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-
-    /* This if/else will display if the user won the game or lost the game. */
-    if (humanScore > computerScore) {
-        console.log(`You won! Congratulations!!!`);
-        return;
-    } else if (computerScore > humanScore) {
-        console.log(`You lost! You'll beat them next time.`);
-    } else {
-        console.log(`It's a tie! No one wins.`);
-    }
+/* This function updates the score */
+function updateScore() {
+    scoreDiv.textContent = `Player: ${humanScore} | Computer: ${computerScore}`;
 }
 
-playGame();
+
+/* This function will check for winner */
+function checkWinner() {
+    if (humanScore === 5) {
+        resultDiv.textContent = "You won the game!";
+    }
+
+    if (computerScore === 5) {
+        resultDiv.textContent = "You lost the game!";
+    }
+}
